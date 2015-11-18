4d8115ea71c09a215ca6a00a520207964b2305f4
Cameron Dawson <cdawson@mozilla.com>
2015-11-18 15:56:46 -0800

    fixup! forgot to revert builds-4hr link

--- treeherder/config/settings.py
+++ treeherder/config/settings.py
@@ -301,7 +301,7 @@ SITE_URL = env("SITE_URL", default="http://local.treeherder.mozilla.org")
 
 BUILDAPI_PENDING_URL = "https://secure.pub.build.mozilla.org/builddata/buildjson/builds-pending.js"
 BUILDAPI_RUNNING_URL = "https://secure.pub.build.mozilla.org/builddata/buildjson/builds-running.js"
-BUILDAPI_BUILDS4H_URL = "https://secure.pub.build.mozilla.org/builddata/buildjson/builds-2015-11-03.js.gz"
+BUILDAPI_BUILDS4H_URL = "https://secure.pub.build.mozilla.org/builddata/buildjson/builds-4hr.js.gz"
 ALLTHETHINGS_URL = "https://secure.pub.build.mozilla.org/builddata/reports/allthethings.json"
 
 # the max size of a posted request to treeherder client during Buildbot

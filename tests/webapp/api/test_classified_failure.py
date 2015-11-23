7fde0250e3df09e27ad3c4601eaf6da35cac6545
James Graham <james@hoppipolla.co.uk>
2015-11-23 12:15:39 +0000

    Reimplement classified_failure endpoint with correct business logic.
    Don't allow multiple failures to have the same bug.
    Allow filtering based on bug number.

---
+++ tests/webapp/api/test_classified_failure.py
@@ -0,0 +1,138 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework.test import APIClient
+
+from treeherder.model.models import ClassifiedFailure
+
+
+def test_get_classified_failure(webapp, classified_failures):
+    """
+    test getting a single failure line
+    """
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = webapp.get(
+        reverse("classified-failure-detail", kwargs={"pk": classified_failures[0].id}))
+
+    assert resp.status_int == 200
+    actual = resp.json
+    expected = {"id": classified_failures[0].id,
+                "bug_number": 1234}
+
+    assert actual == expected
+
+
+def test_get_classified_failures(webapp, classified_failures):
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = webapp.get(reverse("classified-failure-list"))
+    assert resp.status_int == 200
+
+    actual = resp.json
+    expected = [{"id": cf.id,
+                 "bug_number": cf.bug_number} for cf in classified_failures]
+    assert actual == expected
+
+
+def test_post_new_classified_failure(webapp, classified_failures):
+    client = APIClient()
+    user = User.objects.create(username="MyName")
+    client.force_authenticate(user=user)
+
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = client.post(reverse("classified-failure-list"),
+                       {"bug_number": 5678}, format="json")
+
+    assert resp.status_code == 200
+
+    actual = resp.data
+    expected = {"id": classified_failures[-1].id + 1,
+                "bug_number": 5678}
+    assert actual == expected
+
+    obj = ClassifiedFailure.objects.get(id=actual["id"])
+    assert obj.bug_number == 5678
+
+
+def test_post_repeated_classified_failure(webapp, classified_failures):
+    client = APIClient()
+    user = User.objects.create(username="MyName")
+    client.force_authenticate(user=user)
+
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = client.post(reverse("classified-failure-list"),
+                       {"bug_number": 1234}, format="json")
+
+    assert resp.status_code == 200
+
+    actual = resp.data
+    expected = {"id": classified_failures[0].id,
+                "bug_number": 1234}
+    assert actual == expected
+
+
+def test_put_new_bug_number(webapp, classified_failures):
+    client = APIClient()
+    user = User.objects.create(username="MyName")
+    client.force_authenticate(user=user)
+
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = client.put(reverse("classified-failure-detail",
+                              kwargs={"pk": classified_failures[0].id}),
+                      {"bug_number": 5678}, format="json")
+
+    assert resp.status_code == 200
+
+    actual = resp.data
+    expected = {"id": classified_failures[0].id,
+                "bug_number": 5678}
+    assert actual == expected
+
+    classified_failures[0].refresh_from_db()
+    assert classified_failures[0].bug_number == 5678
+
+
+def test_put_existing_bug_number(webapp, classified_failures):
+    client = APIClient()
+    user = User.objects.create(username="MyName")
+    client.force_authenticate(user=user)
+
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = client.put(reverse("classified-failure-detail",
+                              kwargs={"pk": classified_failures[0].id}),
+                      {"bug_number": 1234}, format="json")
+
+    assert resp.status_code == 200
+
+    actual = resp.data
+    expected = {"id": classified_failures[0].id,
+                "bug_number": 1234}
+    assert actual == expected
+
+    classified_failures[0].refresh_from_db()
+    assert classified_failures[0].bug_number == 1234
+
+
+def test_put_duplicate_bug_number(webapp, classified_failures):
+    client = APIClient()
+    user = User.objects.create(username="MyName")
+    client.force_authenticate(user=user)
+
+    classified_failures[0].bug_number = 1234
+    classified_failures[0].save()
+
+    resp = client.put(reverse("classified-failure-detail",
+                              kwargs={"pk": classified_failures[1].id}),
+                      {"bug_number": 1234}, format="json")
+
+    assert resp.status_code == 400
--- treeherder/webapp/api/classifiedfailure.py
+++ treeherder/webapp/api/classifiedfailure.py
@@ -1,11 +1,64 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from treeherder.model.models import ClassifiedFailure
 from treeherder.webapp.api import serializers
 
 
-class ClassifiedFailureViewSet(viewsets.ModelViewSet):
+class ClassifiedFailureViewSet(viewsets.ViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    queryset = ClassifiedFailure.objects.all()
     serializer_class = serializers.ClassifiedFailureSerializer
+
+    def retrieve(self, request, pk=None):
+        try:
+            obj = ClassifiedFailure.objects.get(id=pk)
+        except ClassifiedFailure.DoesNotExist:
+            return Response("No classified failure with id %i" % pk, 500)
+
+        return Response(self.serializer_class(obj).data)
+
+    def list(self, request):
+        queryset = ClassifiedFailure.objects.all()
+        query_params = {}
+        for key, values in self.request.query_params.iteritems():
+            if len(values) > 1:
+                if not key.endswith("__in"):
+                    key += "__in"
+                value = values
+            else:
+                value = values[0]
+            query_params[key] = value
+        queryset = queryset.filter(**query_params)
+        return Response(self.serializer_class(queryset, many=True).data)
+
+    def create(self, request):
+        bug = request.data.get('bug_number')
+        if bug:
+            obj, _ = ClassifiedFailure.objects.get_or_create(bug_number=bug)
+        else:
+            obj = ClassifiedFailure()
+        obj.save()
+        return Response(self.serializer_class(obj).data)
+
+    def update(self, request, pk=None):
+        try:
+            obj = ClassifiedFailure.objects.get(id=pk)
+        except ClassifiedFailure.DoesNotExist:
+            return Response("No classified failure with id: {0}".format(pk), 404)
+
+        bug_number = request.data.get('bug_number')
+        if bug_number is None:
+            return Response("No bug number provided", 400)
+
+        # The other option here would be to merge the classifications.
+        try:
+            existing = ClassifiedFailure.objects.exclude(id=pk).get(bug_number=bug_number)
+            return Response("Bug number %i already used for classification %i" %
+                            (bug_number, existing.id), 400)
+        except ClassifiedFailure.DoesNotExist:
+            pass
+
+        obj.bug_number = bug_number
+        obj.save()
+        return Response(self.serializer_class(obj).data)
